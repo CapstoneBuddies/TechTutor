af62d49d17402dc74f244cbb187fed862817549b
Chromata03 <cajes.jomarie.99@gmail.com>
2025-03-14 00:40:13 +0800

    Fixing Merge Conflicts
    Ambot if naa naba tanan fontawesome files ani

--- pages/admin/view-users.php
+++ pages/admin/view-users.php
@@ -132,7 +132,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h5 class="card-title">User Management</h5>
+                                <h5 class="card-title">User Management (PA HELP KO TT)</h5>
                             </div>
                             <div class="card-body">
                                 <!-- Filter Buttons -->
